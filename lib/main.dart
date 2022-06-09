fcea0a6e12ea2134ade11442e5cb559b5c20485b
Jake Bersani <53477487+JBusani@users.noreply.github.com>
2022-06-10 02:12:33 +0900

    added scaffold, appbar, colors, body with image.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 //lesson 1
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text("I am Rich"),
+        backgroundColor: Colors.lightGreenAccent,
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              "https://media.istockphoto.com/photos/seaside-road-approaching-a-beach-seen-from-above-picture-id1158679440?s=612x612"),
+        ),
       ),
     ),
-  );
+  ));
 }
