7c3bc2f4f474540257adfe9be0e892098547e16d
Jake Bersani <53477487+JBusani@users.noreply.github.com>
2022-06-16 01:47:10 +0900

    importing assets locally, using app generator and using modules to edit display on home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 
 //lesson 1
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: const Text("I am Rich"),
-        backgroundColor: Colors.lightGreenAccent,
-      ),
-      body: const Center(
-        child: Image(
-          image: NetworkImage(
-              "https://media.istockphoto.com/photos/seaside-road-approaching-a-beach-seen-from-above-picture-id1158679440?s=612x612"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text("I am Rich"),
+          backgroundColor: Colors.lightGreenAccent,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
